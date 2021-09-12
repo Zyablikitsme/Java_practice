ce2899e638a1bd932c3b4e928f4a1e04c0e3912c
Kirill <v825ar@gmail.com>
2021-09-12 12:00:35 +0300

    add 3rd practice(not ready)

--- src/ru/mirea/java/practice3/Square.java
+++ src/ru/mirea/java/practice3/Square.java
@@ -6,9 +6,7 @@ public class Square extends Rectangle{
     public Square() {
     }
 
-    public Square(double side) {
-        this.side = side;
-    }
+    public Square(double side) { this.side = side; }
 
     public Square(double side, String color, boolean filled){
         this.side = side;
@@ -16,17 +14,11 @@ public class Square extends Rectangle{
         this.filled = filled;
     }
 
-    public double getSide() {
-        return side;
-    }
+    public double getSide() { return side; }
 
-    public void setSide(double side) {
-        this.side = side;
-    }
+    public void setSide(double side) { this.side = side; }
 
-    public void setWidth(double side){   // ???
-        width = side;
-    } // ???
+    public void setWidth(double side){ width = side; } // ????
 
     public void setLength(double side){ length = side; } // ???
 
