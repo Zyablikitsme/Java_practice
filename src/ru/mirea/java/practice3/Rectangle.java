d62711f3e9913c8c325b0668d368818bd713482d
Kirill <v825ar@gmail.com>
2021-09-12 09:52:20 +0300

    add 3rd practice(not ready)

---
+++ src/ru/mirea/java/practice3/Rectangle.java
@@ -0,0 +1,56 @@
+package ru.mirea.java.practice3;
+
+public class Rectangle extends Shape{
+    protected double width;
+    protected double length;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return 0.5 * width * length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return width + 2*length;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
